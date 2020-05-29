2f9589a96f9aadf44b19bc9762ec300dc583946d
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-29 01:00:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,14 @@
 class CashRegister
   
-  attr_accessor :total, :discount 
+  attr_accessor :total, :discount, :item  
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+  end
+  
+  def add_item(item, price)
+    
   end
   
 end
