0eaecc58c7caffe89fe0cbf7317a0a1d5b0568fd
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-29 01:27:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   
   def add_item(item, price, quantity = 1)
     @total += price * quantity
-    @items << item 
+    @items << item * quantity
   end
   
   def apply_discount
